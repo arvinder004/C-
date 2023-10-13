c5b42dca7917737f7ae2401843a3dd49ef987fb5
Arvinder Singh Dhoul <asdhoul004@gmail.com>
2023-10-13 07:16:41 +0530

    Create sum_using_calloc.cpp

---
+++ sum_using_calloc.cpp
@@ -0,0 +1,25 @@
+//sum of numbers using dynamic memory allocation(calloc function).
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int *arr, n, sum = 0;
+	
+	printf("enter number of values to add:");
+	scanf("%d",&n);
+	
+	arr= (int *)calloc(n, sizeof(int));
+	
+	for(int i=0; i<n; i++){
+		printf("enter %d number:",i+1);
+		scanf("%d",arr+i);
+		
+		sum += *(arr+i);
+	}
+	
+	printf("Sum of %d numbers is:%d",n,sum);
+	
+	free(arr);
+	return 0;
+}
