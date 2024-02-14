524b8092841c1c16c768d253d18cdd6190df85f4
Arvinder Singh Dhoul <asdhoul004@gmail.com>
2024-02-14 22:42:31 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+main(){
+	int num1, num2;
+	
+	cout<<"enter first number:";
+	cin>>num1;
+	
+	cout<<"enter second number:";
+	cin>>num2;
+	
+	cout<<"Addition:"<<num1+num2;
+	
+	
+	return 0;
+}
