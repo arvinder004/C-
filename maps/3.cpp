837014f1c64fb01520db175bd35958c2087ea722
Arvinder Singh Dhoul <asdhoul004@gmail.com>
2024-04-20 22:01:05 +0530

    3

---
+++ maps/3.cpp
@@ -0,0 +1,77 @@
+//unordered maps
+
+#include<iostream>
+#include<string>
+#include<map>
+#include<unordered_map>
+
+using namespace std;
+
+class conversion{
+    public:
+        int RomanToInt(string s){
+            unordered_map<char,int>m;
+                m['I']=1;
+                m['V']=5;
+                m['X']=10;
+                m['L']=50;
+                m['C']=100;
+                m['D']=500;
+                m['M']=1000;
+
+            int num = 0;
+            
+            for(int i=0; i<s.length(); i++){
+                if(m[s[i]] < m[s[i+1]]){
+                    num -= m[s[i]];
+                }
+                else{
+                    num += m[s[i]];
+                }
+            }
+            return num;
+        }
+
+        string IntToRoman(int num){
+            unordered_map<char,int>m;
+                m[1000]='M';
+                m[900]='CM';
+                m[500]='D';
+                m[100]='C';
+                m[90]='XC';
+                m[50]='L';
+                m[10]='X';
+                m[9]='IX';
+                m[5]='V';
+                m[1]='I';
+
+            string roman = "";
+
+            for(int i=0; i<m.size(); i++){
+                while(num >= m[i]){
+                    roman += m[i].
+                }
+            }
+        }
+};
+
+int main(){
+    cout<<"--------------ROMAN INTEGER CONVERSION--------------"<<endl;
+    cout<<"(please enter roman numerals in capital (I,V,X,L,C,D,M))"
+    cout<<"TO CONVERT ROMAN TO INTEGER PRESS 1"<<endl;
+    cout<<"TO CONVERT INTEGER TO ROMAN PRESS 2"<<endl;
+
+    int val;
+    cin>>val;
+
+    if(val == 1){
+        string s;
+        cout<<"ENTER ROMAN NUMBER:"<<endl;
+        cin>>s;
+        conversion c1;
+        cout<<"INTEGER NUMBER : "<<c1.RomanToInt(s);
+    }
+
+    return 0;
+
+}
